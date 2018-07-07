865b944c3b849b7de4571882b71744852fa4542a
Hakan <h.ilbas@student.tudelft.nl>
2018-07-07 22:38:31 +0200

    Added input validator

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -8,14 +8,14 @@ public class RandomNumberGenerator {
 
     public static void launcher(){
         boolean running = true;
+        int random = 0;
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Welcome to the random number generator\n");
 
         while(running){
-            System.out.println("Welcome to the random number generator\n"
-                    + "If you want to stop, type stop in the console, else press enter");
-
+            System.out.println("If you want to stop, type stop in the console, else press enter");
             String stop = sc.nextLine();
+
             if(stop.equals("stop")){
                 System.out.println("Random number generator stopped");
                 running = false;
@@ -24,16 +24,44 @@ public class RandomNumberGenerator {
 
             System.out.println("To generate a number enter a lower and upper bound.");
             System.out.println("Enter lower bound");
-            int firstbound = sc.nextInt();
+            String firstbound = sc.nextLine();
 
             System.out.println("Enter upper bound");
-            int secondbound = sc.nextInt();
+            String secondbound = sc.nextLine();
 
-            int random = firstbound + (int)(Math.random() * ((secondbound - firstbound) + 1));
+            if(inputvalidator(firstbound, secondbound)) {
+                random = Integer.parseInt(firstbound) + (int) (Math.random() * ((Integer.parseInt(secondbound) - Integer.parseInt(firstbound)) + 1));
+            }else{
+                continue;
+            }
 
             System.out.println("The randomly generated number is: " + random);
 
         }
     }
 
+    public static boolean inputvalidator(String lower, String upper){
+        for(char c: lower.toCharArray()){
+            if(!Character.isDigit(c)){
+                System.out.println("Upperbound contains characters which are not digits");
+                System.out.println("Re-enter input");
+                return false;
+            }
+        }
+
+        for(char c: upper.toCharArray()){
+            if(!Character.isDigit(c)){
+                System.out.println("Upperbound contains characters which are not digits");
+                System.out.println("Re-enter input");
+                return false;
+            }
+        }
+
+        if(Integer.parseInt(lower)>Integer.parseInt(upper)){
+            System.out.println("Lowerbound is larger than upperbound");
+            System.out.println("Re-enter input with lower lowerbound");
+            return false;
+        }
+        return true;
+    }
 }
