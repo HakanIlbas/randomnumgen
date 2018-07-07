74d391f8ad786ac3aeaef4515a547241477edb26
Hakan <h.ilbas@student.tudelft.nl>
2018-07-07 21:52:57 +0200

    Added a stop functionality

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -12,19 +12,28 @@ public class RandomNumberGenerator {
 
 
         while(running){
-            System.out.println("Enter first bound");
+            System.out.println("Welcome to the random number generator\n"
+                    + "If you want to stop, type stop in the console, else press enter");
+
+            String stop = sc.nextLine();
+            if(stop.equals("stop")){
+                System.out.println("Random number generator stopped");
+                running = false;
+                continue;
+            }
+
+            System.out.println("To generate a number enter a lower and upper bound.");
+            System.out.println("Enter lower bound");
             int firstbound = sc.nextInt();
 
-            System.out.println("Enter second bound");
+            System.out.println("Enter upper bound");
             int secondbound = sc.nextInt();
 
             int random = firstbound + (int)(Math.random() * ((secondbound - firstbound) + 1));
 
-            System.out.println("Random number: " + random);
-
+            System.out.println("The randomly generated number is: " + random);
 
         }
-
     }
 
 }
